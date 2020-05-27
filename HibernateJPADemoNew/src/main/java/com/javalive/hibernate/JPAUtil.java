e42d3d80fd6492cfa78c9648b7e31a8c155841ad
Administrator <Administrator@DELL>
2020-05-27 14:52:15 +0530

    Initial commit.

---
+++ HibernateJPADemoNew/src/main/java/com/javalive/hibernate/JPAUtil.java
@@ -0,0 +1,25 @@
+package com.javalive.hibernate;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * @author JavaLive.com
+ */
+public class JPAUtil {
+  private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE";
+  private static EntityManagerFactory factory;
+
+  public static EntityManagerFactory getEntityManagerFactory() {
+    if (factory == null) {
+      factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    }
+    return factory;
+  }
+
+  public static void shutdown() {
+    if (factory != null) {
+      factory.close();
+    }
+  }
+}
